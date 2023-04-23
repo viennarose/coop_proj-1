958a7bae53f444f717b2f67188f40a82148c48d4
Kenn Secusana <kennsecusana@gmail.com>
2023-04-23 18:33:51 +0800

    added

--- app/src/main/java/com/bisu/pslat/user/ui/DepositsViewList.java
+++ app/src/main/java/com/bisu/pslat/user/ui/DepositsViewList.java
@@ -3,6 +3,8 @@ package com.bisu.pslat.user.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bisu.pslat.R;
 
@@ -12,5 +14,19 @@ public class DepositsViewList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposits_view_list);
+
+
+        Button backBtn = (Button) findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+
     }
+
 }
